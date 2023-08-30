c9416085e90da7ba5223e9940fd0a31f2e7ad979
coding-to-music <connors.tom@gmail.com>
2023-08-29 21:21:30 -0500

    Build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,20 @@
-import './globals.css'
+import { Analytics } from "@vercel/analytics/react";
+import "./globals.css";
 
 export const metadata = {
-  title: 'cloudinary-image-search-nextjs',
-  description: 'Node SDK Nextjs 13 Cloudinary Image Gallery with Search',
-}
+  title: "cloudinary-image-search-nextjs",
+  description: "Node SDK Nextjs 13 Cloudinary Image Gallery with Search",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
+      <Analytics />
     </html>
-  )
+  );
 }
